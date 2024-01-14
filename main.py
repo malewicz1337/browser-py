84f0045350f32df47f08fa8b45af27450549096b
Name <daniil.adamov.2001@gmail.com>
2024-01-14 17:09:12 -0600

    Fixed browser window)))

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def show(body):
+import tkinter
+
+
+def lex(body):
     """Remove html tags from body and print text only."""
 
+    text = ""
     in_tag = False
     for c in body:
         if c == "<":
@@ -8,24 +12,48 @@ def show(body):
         elif c == ">":
             in_tag = False
         elif not in_tag:
-            print(c, end="")
+            # print(c, end="")
+            text += c
+
+    return text
 
 
-def load(url):
-    """Send the request, recieve and show body."""
+WIDTH, HEIGHT = 800, 600
 
-    try:
-        body = url.request()
-        show(body)
-        return True
-    except Exception as e:
-        print(e)
-        print("Failed to load", url)
-        return False
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+    def load(self, url):
+        """Send the request, recieve and show body."""
+
+        try:
+            body = url.request()
+            text = lex(body)
+
+            HSTEP, VSTEP = 13, 18
+            cursor_x, cursor_y = HSTEP, VSTEP
+            for c in text:
+                self.canvas.create_text(cursor_x, cursor_y, text=c)
+                cursor_x += HSTEP
+
+                if cursor_x >= WIDTH - HSTEP:
+                    cursor_y += VSTEP
+                    cursor_x = HSTEP
+
+            return True
+        except Exception as e:
+            print(e)
+            print("Failed to load", url)
+            return False
 
 
 if __name__ == "__main__":
     import sys
     from url import URL
 
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
